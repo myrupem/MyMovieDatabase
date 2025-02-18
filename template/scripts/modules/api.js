742ee43575d479465b38bfb8ea06192e8280bd76
Emilia Myrup <myrupemilia@gmail.com>
2025-02-18 12:11:58 +0100

    render trailers done

--- template/scripts/modules/api.js
+++ template/scripts/modules/api.js
@@ -1,7 +1,23 @@
-import oData from '../data/data.js';
+//import oData from '../data/data.js';
+import { shuffle } from '../utils/utils.js';
+import log from '../utils/logger.js';
 
-export async function fetchTopMovies() {
-    const response = await fetch('https://santosnr6.github.io/Data/favoritemovies.json');
-    let movies = await response.json();
-    oData.topMovieList = movies;
+export function fetchTopMovies() {
+    return fetch('https://santosnr6.github.io/Data/favoritemovies.json')
+        .then(response => response.json())
+        .then(data => {
+        return data; // Returnerar 38 filmer
+        })
+        .catch(error => {
+            log("Något gick fel: ", error.message)
+        });
 }
+
+//Ska denna va här?
+export async function fetchTrailers() {
+    let movies = await fetchTopMovies()
+    let fiveArray = shuffle(movies)
+    return fiveArray //Returnerad en shufflad array av 5 filmer
+}
+
+
