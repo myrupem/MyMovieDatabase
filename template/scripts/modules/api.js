fe3f454c173cd8b3bb627a690be356db55a8a5bc
Emilia Myrup <myrupemilia@gmail.com>
2025-02-19 21:02:36 +0100

    wideSearch done

--- template/scripts/modules/api.js
+++ template/scripts/modules/api.js
@@ -4,7 +4,7 @@ export function fetchTopMovies() {
     return fetch('https://santosnr6.github.io/Data/favoritemovies.json')
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            log(data)
             return data; // Returnerar 38 filmer
         })
         .catch(error => {
@@ -13,4 +13,31 @@ export function fetchTopMovies() {
         });
 }
 
+export function fetchSearch(input) {
+    return fetch(`http://www.omdbapi.com/?apikey=99013a27&s=${input}*`)
+    .then(response => response.json())
+    .then(data => {
+        log(data.Search); //Returnerar en array med alternativ efter sök. Bred sökning
+        return data.Search;
+    })
+    .catch(error => {
+        log("Något gick fel: ", error.message)
+        return []
+    })
+}
+
+
+//Denna funkar men behöver nu ta emot ett IMDB id
+export function searchSingleMovie(input) {
+    return fetch(`http://www.omdbapi.com/?apikey=99013a27&i=${input}*`)
+    .then(response => response.json())
+    .then(data => {
+        log(data);
+        return data;
+    })
+    .catch(error => {
+        log("Något gick fel: ", error.message)
+        return []
+    })   
+}
 
